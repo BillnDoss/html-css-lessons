e601dcf1e41d4e254a47a431750e2afc861181b2
Jacky Tan <jackytan06028@gmail.com>
2026-04-14 08:57:07 +0800

    added extra comments for bootstrap components

---
+++ js/basic.js
@@ -0,0 +1,36 @@
+console.log("Hello!");
+console.log("Addition");
+console.log(1 + 1);
+console.log("Subtraction");
+console.log(4 - 3);
+console.log("Multiplication");
+console.log(11 * 30);
+console.log("Division");
+console.log("60 / 5");
+
+/*
+    This is a multi-line comment
+    It can span across many lines
+    Just hit enter to go to the next line
+    as long as it is wrapped in /*, it will work.
+*/
+
+// This is a single line comment
+// Use CTRL + / (Windows) OR CMD + / (Mac) for the shortcut
+
+// Try to comment the code below, put your cursor over the code and hit
+// comment shortcut to instantly comment the code
+
+// console.log("Comment out this code!")
+
+// Still remember what comments are?
+// Comments are notes for the developer/coder to help understand the code
+
+// BODMAS / PEMDAS
+// (Brackets Order Divison Multiplication Addition Subtraction) (left to right)
+// (Parentheses Exponent Multiplication Division Addition Subtraction) (right to left)
+// Mathematic power is not ^ but is **
+
+console.log(5 + 3 * 4); // 17
+console.log(5 + 9 ** 2); // 86
+console.log((7 + 2) ** 2 + 45); // 126
---
+++ js/variables.js
@@ -0,0 +1,80 @@
+// allows the value or name to be changed
+var x = 5;
+x = 20;
+console.log(x); // Output: 20
+
+// forces it to be a specific name or value no matter what
+const y = 30;
+//y = 40 //This will produce an error because a is a constant variable
+
+console.log(x + y); // 50
+
+var firstName = "Paulchris";
+var lastName = "Yeow";
+
+console.log(firstName + lastName); //PaulChrisYeow
+console.log(firstName + " " + lastName); //PaulChris Yeow
+
+/* 
+Pirmitive Data Types
+String: Represent text, e.g: your name
+Numbers (Integers): For math computations
+                    Written without quotes (")
+Float (Decimal): Also for math, but is decimal
+Booleans: True or False values (1 or 0)
+*/
+
+// Special Data Type - Undefined and Null
+// Undefined: Before a value is assigned - var x
+// Null: Absence of a value / No Value - var x = null
+// NaN : Not a Number (trying to do something with math that isnt math related)
+// Both are different from 0.
+
+// Non-Primitive Data Types - Arrays and Objects
+// Arrays
+
+// stores a large of amount of data
+// 0 displays the first data
+// arranged in sequence and uses numbers instead of text
+
+var anArray = [1, 2, 3, 4, 5];
+console.log(anArray[0]); // 1
+console.log(anArray[1]); // 2
+console.log(anArray[2]); // 3
+
+var fruits = ["Apple", "Orange", "Banana"];
+console.log(fruits[0]); // Apple
+console.log(fruits[1]); // Orange
+console.log(fruits[2]); // Banana
+
+// Objects
+// no order and uses key value pai
+// key: "value"
+
+// const movie = {
+//     name: "Avengers: Endgame",
+//     year: "2019",
+//     directors: "Anthony Russo, Joe Russo",
+//     runningTime: "3h 1m",
+//     storyBy: "Stan Lee, Jack Kirby, Jim Starlin",
+// };
+console.log(movie.name);
+console.log(movie.year);
+console.log(movie.storyBy);
+
+// Arrays are sequential (arranged in order of numbers)
+// Starts from 0
+// Objects are used by key-value pairs
+// Left Side: Key, Right side: Value
+
+// Objects with Arrays
+// can be converted into arrays by adding square brackets
+// add square brackets behind the constant name and value (position) of the object
+const movie = {
+    name: "Avengers: Endgame",
+    year: "2019",
+    directors: ["Anthony Russo, Joe Russo"],
+    runningTime: "3h 1m",
+    storyBy: ["Stan Lee, Jack Kirby, Jim Starlin"],
+};
+console.log(movie.directors[0]);
